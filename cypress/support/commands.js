7c3626ab79a4cf7b14272d2b0a327707f17d06b1
NikitaBez <bezmenov.nikita@gmail.com>
2023-06-18 23:34:19 +0900

    test on books.cy.js and commands.js is modefied
    сделаны основные работы по написанию тестов

---
+++ cypress/support/commands.js
@@ -0,0 +1,65 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import "cypress-file-upload";
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.contains("Log in").click();
+  if (email) {
+    cy.get("#mail").type(email);
+  }
+  if (password) {
+    cy.get("#pass").type(password);
+  }
+  cy.contains("Submit").click();
+});
+
+Cypress.Commands.add("addBook", (title, description, authors) => {
+  cy.contains("Add new").click();
+  if (title) {
+    cy.get("#title").type(title);
+  }
+  if (description) {
+    cy.get("#description").type(description);
+  }
+
+  if (authors) {
+    cy.get("#authors").type(authors);
+  }
+
+});
+
+Cypress.Commands.add("deleteBook", (title, authors) => {
+  cy.contains("Favorites").click();
+  if (title) {
+    cy.get("#.card-title").type(title);
+  }
+
+  if (authors) {
+    cy.get("#.card-text").type(authors);
+  }
+  cy.get("#.btn").click();
+});
